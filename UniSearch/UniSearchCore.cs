5759bdf785d8ad1dab9243324800cf9e68f0b69e
Kolandor <pyelya@gmail.com>
2017-10-17 22:37:16 +0300

    Search Core ready

--- UniSearch/UniSearchCore.cs
+++ UniSearch/UniSearchCore.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -7,37 +11,32 @@ namespace UniSearch
 {
     public class UniSearchCore
     {
-        //Working stable regular expressions
-        //Public pattern
-        //Regex regex = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)");
-        //My complicated pattern
-        //Regex regex = new Regex(@"https?://[\w\d\-_]+(\.[\w\d\-_]+)+[\w\d\-\.,@?^=%&amp;:/~\+#]*");
-        //My simple pattern
-        //Regex regex = new Regex(@"https?://[\w\d-\._~:/?#\[\]@!$&'()*+,;=`]+");
-
-
-
         private ListView _searchInfoListView;
 
         private object _criticalSection = true;
 
-        private Thread _mainThread = null;
+        private Thread _mainThread;
 
         public UniSearchCore(ListView listView)
         {
             _searchInfoListView = listView;
         }
 
-        public void Start(string urlRoot, int searchDeep, int threadCount)
+        public void Start(string urlRoot, int targetDeep, int threadCount, string searchString)
         {
-            Tuple<string, int, int> tuple = new Tuple<string, int, int>(urlRoot, searchDeep, threadCount);
+            Tuple<string, int, int, string> tuple = 
+                new Tuple<string, int, int, string>(urlRoot, targetDeep, threadCount, searchString);
             _mainThread = new Thread(Process) { IsBackground = true };
             _mainThread.Start(tuple);
         }
 
         public void Stop()
         {
-
+            if (_mainThread != null)
+            {
+                _mainThread.Abort();
+                _mainThread = null;
+            }
         }
 
         public void Pause()
@@ -49,7 +48,7 @@ namespace UniSearch
         {
             try
             {
-                Tuple<string, int, int> paramsTuple = objTupleParams as Tuple<string, int, int>;
+                Tuple<string, int, int, string> paramsTuple = objTupleParams as Tuple<string, int, int, string>;
 
                 if (paramsTuple == null)
                 {
@@ -57,8 +56,9 @@ namespace UniSearch
                 }
 
                 string urlRoot = paramsTuple.Item1;
-                int searchDeep = paramsTuple.Item2;
+                int targetDeep = paramsTuple.Item2;
                 int threadCount = paramsTuple.Item3;
+                string searchString = paramsTuple.Item4;
 
                 List<OngoingThread> poolThreads = new List<OngoingThread>();
 
@@ -67,7 +67,28 @@ namespace UniSearch
                     poolThreads.Add(new OngoingThread(SearchMethod));
                 }
 
+                List<string> scanned = new List<string>();
+                List<string> scan = new List<string> { urlRoot };
 
+                while (scan.Count > 0 && scanned.Count <= targetDeep)
+                {
+                    List<string> nextScan = new List<string>();
+
+                    for (int i = 0; i < scan.Count; i++)
+                    {
+                        Tuple<string, List<string>, List<string>, string> tupleScanData =
+                            new Tuple<string, List<string>, List<string>, string>(scan[i], scanned, nextScan, searchString);
+
+                        poolThreads[i % poolThreads.Count].Start(tupleScanData);
+                    }
+
+                    foreach (OngoingThread thread in poolThreads)
+                    {
+                        thread.Join();
+                    }
+
+                    scan = nextScan;
+                }
             }
             catch (Exception exception)
             {
@@ -77,7 +98,81 @@ namespace UniSearch
 
         void SearchMethod(object objTupleParams)
         {
-            Tuple<string, List<string>, List<string>> paramsTuple = objTupleParams as Tuple<string, List<string>, List<string>>;
+            ListViewItem item = new ListViewItem();
+
+            lock (_criticalSection)
+            {
+                _searchInfoListView.Items.Add(item);
+            }
+
+            try
+            {
+                Tuple<string, List<string>, List<string>, string> paramsTuple = 
+                    objTupleParams as Tuple<string, List<string>, List<string>, string>;
+
+                if (paramsTuple == null)
+                {
+                    throw new Exception("Wrong thread search params.");
+                }
+
+                string currentUrl = paramsTuple.Item1;
+                List<string> scanned = paramsTuple.Item2;
+                List<string> nextScan = paramsTuple.Item3;
+                string searchString = paramsTuple.Item4;
+
+                item.Text += currentUrl;
+
+                lock (_criticalSection)
+                {
+                    scanned.Add(currentUrl);
+                }
+
+                string html = string.Empty;
+
+                HttpWebRequest request = WebRequest.CreateHttp(currentUrl);
+                using (WebResponse response = request.GetResponse())
+                {
+                    Stream stream = response.GetResponseStream();
+                    if (stream == null)
+                    {
+                        throw new Exception("can't get web page.");
+                    }
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        html = reader.ReadToEnd();
+                        if (html.Length == 0)
+                        {
+                            throw new Exception("can't get web page html.");
+                        }
+                    }
+                    stream.Close();
+                }
+
+                html = html.ToLower();
+
+                item.Text += @" Concurrences: " + Regex.Matches(html, searchString.ToLower()).Count;
+
+                //Working stable regular expressions
+                //Public pattern
+                //Regex regex = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)");
+                //My complicated pattern
+                //Regex regex = new Regex(@"https?://[\w\d\-_]+(\.[\w\d\-_]+)+[\w\d\-\.,@?^=%&amp;:/~\+#]*");
+                //My simple pattern
+                //Regex regex = new Regex(@"https?://[\w\d-\._~:/?#\[\]@!$&'()*+,;=`]+");
+
+                Regex regex = new Regex(@"https?://[\w\d\-_]+(\.[\w\d\-_]+)+[\w\d\-\.,@?^=%&amp;:/~\+#]*");
+
+                MatchCollection matches = regex.Matches(html);
+
+                lock (_criticalSection)
+                {
+                    nextScan.AddRange(from Match match in matches where !scanned.Contains(match.Value) select match.Value);
+                }
+            }
+            catch (Exception exception)
+            {
+                item.Text += @" Search error: " + exception.Message;
+            }
         }
     }
 }
